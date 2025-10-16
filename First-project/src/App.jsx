302bff6f49a1ab745c3e14a720862396db1ccc1d
sukhveer singh <sukhveersingh.17052005@gmail.com.com>
2025-10-16 10:06:33 +0530

    MYA

--- First-project/src/App.jsx
+++ First-project/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  
 
   return (
     <>
@@ -14,12 +15,19 @@ function App() {
         </a>
        
       </div>
-      <h1>Basic re</h1>
-      <h1>single page app</h1>
-      <h1>randaring</h1>
-      <h1>DOM Surtu</h1>
-      <h1>jsx</h1>
-      <h1>sliding</h1>
+      <h1 className="had">React Tutorial  Basic</h1>
+      <ul>
+      <h2><a href="#"> What is React?</a></h2>
+      <h2><a href="#">Setting up the Environment</a></h2>
+      <h2><a href="#">JSX Introduction</a></h2>
+      <h2><a href="#">Components in React</a></h2>
+      <h2><a href="#">Props and State</a></h2>
+      <h2><a href="#">Handling Events</a></h2>
+      <h2><a href="#">Conditional Rendering</a></h2>
+      <h2><a href="#">Lists and Keys</a></h2>
+      <h2><a href="#">React Hooks (useState, useEffect)</a></h2>
+      <h2><a href="#">React Router Basics</a></h2>
+      </ul>
       
       
     </>
