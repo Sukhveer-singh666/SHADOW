78f7812c1cdc0c83f059357776f65a932ccea85e
sukhveer singh <sukhveersingh.17052005@gmail.com.com>
2025-10-16 09:00:32 +0530

    MYA

---
+++ First-project/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+       
+      </div>
+      <h1>Basic re</h1>
+      <h1>single page app</h1>
+      <h1>randaring</h1>
+      <h1>DOM Surtu</h1>
+      <h1>jsx</h1>
+      <h1>sliding</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
