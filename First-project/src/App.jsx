74120970feb5fb13ddceb0a99e84254a74d5d55c
sukhveer singh <sukhveersingh.17052005@gmail.com.com>
2025-11-13 09:53:52 +0530

    MYprp

--- First-project/src/App.jsx
+++ First-project/src/App.jsx
@@ -4,28 +4,61 @@ import viteLogo from '/react.svg'
 import './App.css'
 // import Car from './vehicle1.jsx';
 // import Cards from './Cards.jsx';
-import Navbar from './Navbar.jsx';
-import About from './About.jsx';
-import Products from './Products.jsx';
-import Cart from './Cart.jsx';
-import Contact from './Contact.jsx';
-import Footer from './Footer.jsx';
-import Card from './Card.jsx';
+import{ Routes, Route,Link} from 'react-router-dom';
+import Home from './Component/home.jsx';
+import About from './Component/About.jsx';
+import Contact from './Component/Contact.jsx';
+
+
+// import Navbar from './Navbar.jsx';
+// import About from './About.jsx';
+// import Products from './Products.jsx';
+// import Cart from './Cart.jsx';
+// import Contact from './Contact.jsx';
+// import Footer from './Footer.jsx';
+// import Card from './Card.jsx';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
   
 
   return (
     
     <>
-    <div><Navbar/></div>
+    <div style={{ textAlign: "center" }}>
+
+      <h1>React Router Demo</h1>
+
+      <nav style={{ marginBottom: "20px" }}>
+        <Link to="/" style={{ margin: "10px" }}>Home</Link>
+        <Link to="/about" style={{ margin: "10px" }}>About</Link>
+        <Link to="/contact" style={{ margin: "10px" }}>Contact</Link>
+      </nav>
+
+       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+    </div>
+
+
+
+
+
+    {/* <div><Navbar/></div>
     <div><About/></div>
     <div><Products/></div>
     <div><Cart/></div>
     <div><Contact/></div>
-    <div><Footer/></div>
-    <div className="A">
+    <div><Footer/></div> */}
+
+
+
+
+    {/* <div className="A">
       <Card image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa7ECW4xHgJyeIQexegqGhxgHGknkobmD0xA&s"
       title="React Basics"
       description="Learn about compoments,props,and state."/>
@@ -37,7 +70,12 @@ function App() {
       description="Learn about compoments,props,and state."/>
 
     </div>
-    
+     */}
+
+
+
+
+
     {/* <div>
       <Card image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa7ECW4xHgJyeIQexegqGhxgHGknkobmD0xA&s"
       title="React Basics"
@@ -45,12 +83,20 @@ function App() {
     </div> */}
 
     
+
+
+
       {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
        
       </div> */}
+
+
+
+
+
       {/* <h1 className="had">React Tutorial  Basic</h1>
       <ul>
       <h2><a href="#"> What is React?</a></h2>
@@ -65,8 +111,14 @@ function App() {
       <h2><a href="#">React Router Basics</a></h2>
       </ul> */}
       
+
+
+
+
       {/* <Car/>
       <Cards/> */}
+
+
       
     </>
   )
